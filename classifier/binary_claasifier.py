3654b0fc8f0f1c968dcc4119154f58db47b042b2
aalinc9 <chingheng113@gmail.com>
2019-05-02 18:21:18 -0400

    back up

--- classifier/binary_claasifier.py
+++ classifier/binary_claasifier.py
@@ -4,13 +4,14 @@ from sklearn.utils import shuffle
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
-import os
+import os, sys
+sys.path.append(os.path.abspath(os.path.join('/data/linc9/tsr_er/')))
 from data import data_util, selected_columns
 
 data = pd.read_csv(os.path.join('..', 'data', 'tsr_er.csv'))
 data = data_util.get_binary_data(data)
-
 data = shuffle(data)
+
 id_data = data[['ICASE_ID', 'IDCASE_ID']]
 y_data = data[['ICD_ID']]
 X_data = data.drop(['ICASE_ID', 'IDCASE_ID', 'ICD_ID'], axis=1)
@@ -21,8 +22,8 @@ print(y_train[y_train.ICD_ID == 0].shape[0]/y_train.shape[0])
 id_train, X_train, y_train = data_util.get_binary_Tomek_Links_cleaned_data(id_train, X_train, y_train)
 print(y_train[y_train.ICD_ID == 0].shape[0]/y_train.shape[0])
 #
-clf = SVC(kernel='rbf', random_state=42, class_weight='balanced')
-# clf = RandomForestClassifier(n_estimators=100, random_state=123)
+# clf = SVC(kernel='rbf', random_state=42, class_weight='balanced')
+clf = RandomForestClassifier(n_estimators=100, class_weight='balanced', random_state=123)
 clf.fit(X_train, y_train)
 X_test = data_util.normalization_onehotcoding_for_testing(X_test, scaler)
 y_predict = clf.predict(X_test)
