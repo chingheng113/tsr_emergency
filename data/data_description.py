ec614af61f54103280d8c488cb30cc51102e2677
aalinc9 <chingheng113@gmail.com>
2019-06-05 23:52:57 -0400

    back up

--- data/data_description.py
+++ data/data_description.py
@@ -11,26 +11,7 @@ data = pd.read_csv(os.path.join('..', 'data', 'tsr_er_og.csv'))
 df_is = data[(data.ICD_ID == 1) | (data.ICD_ID == 2)]
 df_he = data[(data.ICD_ID == 3) | (data.ICD_ID == 4)]
 
-# print(data.shape[0])
-# print(df_is.shape[0])
-# print(df_he.shape[0])
-# print(data[data.GENDER_TX == 0].shape[0])
-# print(data[data.GENDER_TX == 0].shape[0]/data.shape[0])
-#
-# print(df_is[df_is.GENDER_TX == 0].shape[0])
-# print(df_is[df_is.GENDER_TX == 0].shape[0]/df_is.shape[0])
-#
-# print(df_he[df_he.GENDER_TX == 0].shape[0])
-# print(df_he[df_he.GENDER_TX == 0].shape[0]/df_he.shape[0])
-a='HBAC_NM'
-# print(data[a].dropna().shape[0])
-# print(data[a].dropna().mean(), data[a].dropna().std())
-# print(df_is[a].dropna().shape[0])
-# print(df_is[a].dropna().mean(), df_is[a].dropna().std())
-# print(df_he[a].dropna().shape[0])
-# print(df_he[a].dropna().mean(), df_he[a].dropna().std())
-
-b ='PAD_ID'
-print(data[data[b] ==0].shape[0])
-print(df_is[df_is[b] ==0].shape[0])
-print(df_he[df_he[b] ==0].shape[0])
+data.drop(['ICASE_ID', 'IDCASE_ID', 'ICD_ID'], axis=1, inplace=True)
+f, ax = plt.subplots(figsize=(18,18))
+sns.heatmap(data.corr(), annot=True)
+plt.show()
